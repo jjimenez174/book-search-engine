b726039e413014bc360805f953822b0f06029078
Jessica Jimenez <jjimenez174@yahoo.com>
2024-06-09 12:32:15 -0700

    change connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,7 @@
-async function connectToDatabase() {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('Mongoose connected to the database');
-    } catch (error) {
-      console.error('Mongoose connection error:', error);
-    }
-  }
-  
-  connectToDatabase();
-  
-  module.exports = mongoose.connection;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+module.exports = mongoose.connection;
+
+
