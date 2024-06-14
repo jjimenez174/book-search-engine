639904355bc7bea5b40baedd1118eba2490676ee
Jessica Jimenez <jjimenez174@yahoo.com>
2024-06-13 19:18:34 -0700

    updated files

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const startApolloServer = async () => {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      const indexPath = path.resolve(__dirname, '../client/dist/index.html');
     });
   }
 
